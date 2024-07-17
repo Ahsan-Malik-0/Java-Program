601a58858b9d08fa63989765120d155c507892ec
Ahsan-Malik-0 <m.ahsan.malik951@gmail.com>
2024-07-17 13:04:49 +0500

    Comma Seperated

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,21 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        for (int i = 0; i < 10; i++) {
-            System.out.print(i + ", ");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i);
+            if (i < 10){
+                System.out.print(", ");
+            }
         }
 
-        for (int i = 0; i < 10; i++) {
-            System.out.print(i + ", ");
+        System.out.println();
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.print(i);
+            if (i > 1) {
+                System.out.print(", "); 
+            }
+            
         }
     }
 }
