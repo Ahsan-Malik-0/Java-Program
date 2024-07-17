3883d316095bb18ebd468ae66352e970ae66fd08
Ahsan-Malik-0 <m.ahsan.malik951@gmail.com>
2024-07-17 13:08:11 +0500

    Add Comments

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
+
+        // Output 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
         for (int i = 1; i <= 10; i++) {
             System.out.print(i);
             if (i < 10){
@@ -9,8 +11,11 @@ public class Main {
             }
         }
 
+        // Next Line
         System.out.println();
 
+
+        // Output 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
         for (int i = 10; i >= 1; i--) {
             System.out.print(i);
             if (i > 1) {
