89d3ac33a5dde67fc1da9ae7c00b5203a25ab911
Ahsan-Malik-0 <m.ahsan.malik951@gmail.com>
2024-07-19 10:17:19 +0500

    Add name

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String name = "Ahsan";
+        System.out.println(name);
 
 
         // Output 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
